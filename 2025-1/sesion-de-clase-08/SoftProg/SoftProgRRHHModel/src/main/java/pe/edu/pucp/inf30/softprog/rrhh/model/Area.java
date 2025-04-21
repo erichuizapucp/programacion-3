c4d1622b7056d04ae444caed238ade77dff79263
erichuizapucp <eric.huiza@pucp.edu.pe>
2025-04-20 19:05:06 -0700

    ejercicios desarrollados en clase

---
+++ 2025-1/sesion-de-clase-08/SoftProg/SoftProgRRHHModel/src/main/java/pe/edu/pucp/inf30/softprog/rrhh/model/Area.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.inf30.softprog.rrhh.model;
+
+public class Area extends ModeloBase {
+    private String nombre;    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
