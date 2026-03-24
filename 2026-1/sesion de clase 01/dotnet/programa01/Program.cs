fac490849fa38e3778d6728a6e0c138ee314929c
erichuizapucp <eric.huiza@pucp.edu.pe>
2026-03-23 21:34:39 -0500

    ejercicios desarrollados en la sesión de clase 01 / 2026-1

---
+++ 2026-1/sesion de clase 01/dotnet/programa01/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] nombres = ["Juan", "María", "Pedro", "Ana", "Luis"];
+        foreach (string nombre in nombres)
+        {
+            Console.WriteLine("Hola, " + nombre);
+        }
+    }
+}
+
---
+++ 2026-1/sesion de clase 01/java/Programa.java
@@ -0,0 +1,11 @@
+
+public class Programa {
+
+    public static void main(String[] args) {
+        String[] nombres = {"Juan", "María", "Pedro"};
+
+        for (String nombre : nombres) {
+            System.out.println("Hola, " + nombre);
+        }
+    }
+}
