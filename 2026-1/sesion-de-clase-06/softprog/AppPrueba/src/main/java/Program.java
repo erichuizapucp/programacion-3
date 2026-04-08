323bfe53e4c0130788e1da4ac3fc83c3c69eb647
erichuizapucp <eric.huiza@pucp.edu.pe>
2026-04-07 22:23:43 -0500

    ejercicio desarrollado en clase

---
+++ 2026-1/sesion-de-clase-06/softprog/AppPrueba/src/main/java/Program.java
@@ -0,0 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class Program {
+    public static void main(String[] args) {
+        ResourceBundle db = ResourceBundle.getBundle("db");
+
+        String host = db.getString("db.host");
+        int port = Integer.parseInt(db.getString("db.puerto"));
+        String esquema = db.getString("db.esquema");
+        String usuario = db.getString("db.usuario");
+        String password = db.getString("db.password");
+
+        String url = "jdbc:mysql://" + host + ":" + port + "/" + esquema;
+
+        try (Connection connection = DriverManager.getConnection(url, usuario, password)) {
+            if (connection != null && !connection.isClosed()) {
+                System.out.println("Conexión con la base de datos exitosa!");
+            }
+        } catch (SQLException e) {
+            System.err.println("Error de conectividad JDBC:");
+        }
+    }
+}
