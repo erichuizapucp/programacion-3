a2e2c7a3baee83e731b372a912ecfc924697edc4
erichuizapucp <eric.huiza@pucp.edu.pe>
2026-09-21 19:36:57 -0700

    se hizo una mejora en la clase DBManager, se reemplazo al operador de fusion nula por el operador de asignacion de fusion nula

--- 2026-2/sesion-de-clase-10/SoftProg/SoftProg.DbManager/DBManager.cs
+++ 2026-2/sesion-de-clase-10/SoftProg/SoftProg.DbManager/DBManager.cs
@@ -6,24 +6,22 @@ namespace SoftProg.DbManager {
     public class DBManager {
         protected string CadenaConexion { get; }
 
-        private static DBManager Instance { get; set; }
+        private static DBManager? Instance { get; set; }
         private static readonly Lock Candado = new();
 
         protected DBManager(string cadenaConexion) { 
-            ArgumentNullException.ThrowIfNullOrEmpty(cadenaConexion, 
-                nameof(cadenaConexion));
-
+            ArgumentNullException.ThrowIfNullOrEmpty(cadenaConexion, nameof(cadenaConexion));
             CadenaConexion = cadenaConexion;
         }
 
         public static DBManager GetInstance() {
             lock (Candado) {
-                return Instance ?? CrearInstancia();
+                return Instance ??= CrearInstancia();
             }
         }
 
         public DbConnection GetConnection() {
-            var conn = new MySqlConnection(CadenaConexion);
+            DbConnection conn = new MySqlConnection(CadenaConexion);
             conn.Open();
             return conn;
         }
@@ -33,11 +31,10 @@ namespace SoftProg.DbManager {
                 .SetBasePath(AppContext.BaseDirectory)
                 .AddJsonFile("appsettings.json").Build();
 
-            string cadenaConexion = builder.GetConnectionString("softprog");
+            string cadenaConexion = builder.GetConnectionString("softprog") ?? 
+                throw new InvalidOperationException("No se pudo encontrar la cadena de conexion");
 
             return new DBManager(cadenaConexion);
         }
-
-
     }
 }
